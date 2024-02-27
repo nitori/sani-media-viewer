eec54676133bbc60b404f01ccefd8e877fcf8d09
Nitori <nitori@chireiden.net>
2024-02-27 15:08:12 +0100

    Fix sorting and selecting and stuff

--- src/components/Files.vue
+++ src/components/Files.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="files">
-    <div v-for="file in sortedFiles">
+    <div v-for="file in files">
       <a href="#" :class="[file.path === current?.path ? 'active' : '']"
          @click.prevent="$emit('select:media', file)">{{ file.name }}</a>
     </div>
@@ -8,46 +8,15 @@
 </template>
 
 <script setup lang="ts">
-import type {FileEntry, ViewerOptions} from "../types";
-import {computed} from "vue";
+import type {FileEntry} from "../types";
 
-const props = defineProps<{
+defineProps<{
   files: FileEntry[],
-  current: FileEntry | null,
-  options: ViewerOptions
+  current: FileEntry | null
 }>();
 
 defineEmits<
     (e: 'select:media', media: FileEntry) => void
 >();
-
-
-const sortedFiles = computed(() => {
-  let files = [...props.files].filter(f => {
-    if (f.name.startsWith('.') && !props.options.showHidden) {
-      return false;
-    }
-    return true;
-  });
-
-  const getVal = (file: FileEntry) => {
-    if (props.options.sortBy === 'm') {
-      return file.mtime;
-    }
-    return file.name.toLowerCase().replace(/[\[\]\(\)\{}<>.]+/g, '');
-  }
-
-  return files.sort((a, b) => {
-    let aVal = getVal(a);
-    let bVal = getVal(b);
-    if (aVal === bVal) {
-      return 0;
-    }
-    if (aVal < bVal) {
-      return props.options.sortReverse ? 1 : -1;
-    }
-    return props.options.sortReverse ? -1 : 1;
-  })
-});
 
 </script>
