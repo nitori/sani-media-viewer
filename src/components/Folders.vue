5a00b1bfb9fdaa03227c954e287695d396aade9b
Lars Peter Søndergaard <l.soendergaard@lichtflut-medien.de>
2024-02-27 08:11:16 +0100

    Add sorting features

--- src/components/Folders.vue
+++ src/components/Folders.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="folders">
-    <div v-for="folder in folders">
+    <div v-for="folder in sortedFolders">
       <a href="#" :class="[folder.path === previous?.path ? 'active' : '']"
          @click.prevent="$emit('select:folder', folder)">{{ folder.name }}</a>
     </div>
@@ -10,8 +10,9 @@
 
 <script setup lang="ts">
 import type {FolderEntry, ViewerOptions} from "../types";
+import {computed} from "vue";
 
-defineProps<{
+const props = defineProps<{
   folders: FolderEntry[],
   previous: FolderEntry | null
   options: ViewerOptions
@@ -20,5 +21,27 @@ defineProps<{
 defineEmits<
     (e: 'select:folder', folder: FolderEntry) => void
 >();
+
+
+const sortedFolders = computed(() => {
+  let folders = [...props.folders].filter(f => {
+    if (f.name != '..' && f.name.startsWith('.') && !props.options.showHidden) {
+      return false;
+    }
+    return true;
+  });
+
+  return folders.sort((a, b) => {
+    let aVal = a.name;
+    let bVal = b.name;
+    aVal = aVal.toLowerCase().replace(/[\[\]\(\)\{}<>.]+/g, '');
+    bVal = bVal.toLowerCase().replace(/[\[\]\(\)\{}<>.]+/g, '');
+    if (aVal === '..') return -1;
+    if (bVal === '..') return 1;
+    if (aVal === bVal) return 0;
+    if (aVal < bVal) return -1;
+    return 1;
+  });
+});
 
 </script>
